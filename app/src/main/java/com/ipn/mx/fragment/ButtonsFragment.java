a96647e0512a27405d8fc669f63c05cfd133eae2
Maozans <elfensunrise@gmail.com>
2019-04-04 01:40:21 -0600

    Se modificó la aplicación para mostrar 2 fragmentos; el mapa y el de botones.

---
+++ app/src/main/java/com/ipn/mx/fragment/ButtonsFragment.java
@@ -0,0 +1,32 @@
+package com.ipn.mx.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ipn.mx.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ButtonsFragment extends Fragment {
+
+
+    public ButtonsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_buttons, container, false);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
